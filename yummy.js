9ed635ce85ce2fbeb5a2fd4fdc6f3da95c38adf7
gahyeonsan <lyuhui87@naver.com>
2024-10-09 14:34:54 +0900

    Update yummy.js

--- yummy.js
+++ yummy.js
@@ -4,21 +4,21 @@
 let table = document.querySelector("#content-table");
 let rows = table.querySelectorAll("tr");
 
-// Create a helper function to shuffle an array or iterable
+// 도우미 기능을 만들어 어레이 또는 반복 가능한 어레이를 셔플합니다
 function shuffle (iterable) {
   // convert array-like objects into arrays
   let array = Array.from(iterable);
   
   // TODO: Shuffle the array
+  //  Use an array shuffling algorithm, such as fisher-yates
   
   return array;
 }
 
-
 let randomRows = shuffle(rows);
 
 // Clear the contents of the table
-// table.innerHTML = "";
+table.innerHTML = "";
 
 // TODO: Iterate over each row of randomRows
 //   append each row to table
