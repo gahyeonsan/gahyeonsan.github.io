f4918e6dda3344554c78ad5ff7af3ff5f770ccb8
gahyeonsan <lyuhui87@naver.com>
2024-10-09 14:42:22 +0900

    Update yummy.js

--- yummy.js
+++ yummy.js
@@ -1,24 +1,24 @@
 // https://www.reddit.com/r/webdev/comments/k0tjax/is_there_to_make_an_html_table_load_with_rows_in/
 
 // Create references to table and table rows
-let table = document.querySelector("#content-table");
-let rows = table.querySelectorAll("tr");
+let tbody = document.querySelector("#randomized-table");
+let rows = tbody.querySelectorAll("tr");
 
-// 도우미 기능을 만들어 어레이 또는 반복 가능한 어레이를 셔플합니다
+// Create a helper function to shuffle an array or iterable
 function shuffle (iterable) {
   // convert array-like objects into arrays
   let array = Array.from(iterable);
   
   // TODO: Shuffle the array
-  //  Use an array shuffling algorithm, such as fisher-yates
   
   return array;
 }
 
+
 let randomRows = shuffle(rows);
 
 // Clear the contents of the table
-table.innerHTML = "";
+// table.innerHTML = "";
 
 // TODO: Iterate over each row of randomRows
 //   append each row to table
